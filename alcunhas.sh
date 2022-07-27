631c4bb0de28fd0244e9beb66b66386000aab868
Roman Imankulov <roman.imankulov@gmail.com>
2022-07-27 21:23:15 +0100

    Criar alcunhas.sh

---
+++ alcunhas.sh
@@ -0,0 +1,29 @@
+alias alcunha=alias
+
+# cria pasta
+alcunha crpasta=mkdir
+
+# muda pasta
+alcunha mdpasta=cd
+
+# tira pasta
+alcunha trpasta=rmdir
+
+# outros comandos
+alcunha toca=touch
+alcunha mata=kill
+alcunha gato=cat
+alcunha x=sh
+alcunha pancada=bash
+
+# malandro
+alcunha malandro=git
+
+# alconhas para malandro
+malandro config --global alias.começa init
+malandro config --global alias.confere checkout
+malandro config --global alias.puxa pull
+malandro config --global alias.empurra push
+malandro config --global alias.comete commit
+malandro config --global alias.adiciona add
+malandro config --global alias.estado status
